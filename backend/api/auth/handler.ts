efefd81fa9ea8804f4471c4751f9e33e758428a7
Stanislav <snikolenko@flo.team>
2023-02-20 12:12:16 +0400

    fix(gallery): fix no principalId error
    fix no principalId error
    
    MODULE-3-PART-1-sls

--- backend/api/auth/handler.ts
+++ backend/api/auth/handler.ts
@@ -9,18 +9,22 @@ const secret = process.env.SECRET;
 // See: https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-lambda-authorizer.html#http-api-lambda-authorizer.v2
 export const httpApiPolicy: APIGatewayTokenAuthorizerWithContextHandler<Record<string, any>> = async (event) => {
   log('httpApiPolicy authorizer is triggered');
-  const token = event['headers'].Authorization;
-  const decodedToken = jwt.verify(token, secret);
-
-  if (token === 'error') {
-    throw new Error('Internal server error');
-  }
-
-  if (!decodedToken) {
+  try {
+    const token = event['headers'].Authorization;
+    if (token === 'null') {
+      return UNAUTHORIZED;
+    }
+    const decodedToken = jwt.verify(token, secret);
+    if (!decodedToken) {
+      return UNAUTHORIZED;
+    }
+    if (token === 'error') {
+      return new Error('Internal server error');
+    }
+    return generatePolicy('user', 'Allow', '*', {});
+  } catch (e) {
     throw UNAUTHORIZED;
   }
-
-  return generatePolicy('user', 'Allow', '*', {});
 };
 
 export function generatePolicy<C extends APIGatewayAuthorizerResult['context']>(
