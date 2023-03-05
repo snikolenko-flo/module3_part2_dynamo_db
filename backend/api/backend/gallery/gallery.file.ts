6e40223440d1bc5163f757528484f89c312e710b
Stanislav <snikolenko@flo.team>
2023-03-05 20:06:51 +0400

    refactor(gallery): simplify the getGallery function
    simplify the getGallery function
    
    MODULE-3-PART-1-sls

--- backend/api/backend/gallery/gallery.file.ts
+++ backend/api/backend/gallery/gallery.file.ts
@@ -1,7 +1,11 @@
+import { createResponse } from '@helper/http-api/response';
+import { log } from '@helper/logger';
 import mongoose from 'mongoose';
 import { PER_PAGE } from '../data/constants.js';
 import { opendir, stat } from 'fs/promises';
 import { Image } from '../models/image.model';
+import { DbService } from '../services/db-service';
+import { IResponseWithImages } from '../interfaces/response';
 
 const mongoUrl = process.env.MONGO_URL;
 
@@ -40,7 +44,37 @@ export class GalleryFile {
     return isDir.isDirectory();
   }
 
-  async getNumberOfPages(limit: number): Promise<number> {
+  isPagesAmountValid(pagesAmount: number, pageNumber: number): boolean {
+    if (pageNumber > pagesAmount || pageNumber <= 0) {
+      log(`The page number ${pageNumber} is wrong.`);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  async getImages(
+    pageNumber: number,
+    pageLimit: number,
+    pagesAmount: number,
+    dbService: DbService,
+    user?: string
+  ): Promise<IResponseWithImages> {
+    if (user) {
+      log(`Get images for the user ${user}.`);
+      return await dbService.getUserImages(pageNumber, pageLimit, pagesAmount, user);
+    } else {
+      log('Get all images.');
+      return await dbService.getImages(pageNumber, pageLimit, pagesAmount);
+    }
+  }
+
+  async getNumberOfPages(limit: number, dbService: DbService, user?: string): Promise<number> {
+    if (user) {
+      const userImagesNumber = await dbService.getUserImagesNumber(user, limit);
+      return this.getNumberOfPagesForUser(userImagesNumber);
+    }
+
     const total = await this.getFilesAmountFromDb();
     const totalPages = this.calculatePagesNumber(total);
 
--- backend/api/backend/gallery/gallery.manager.ts
+++ backend/api/backend/gallery/gallery.manager.ts
@@ -1,5 +1,4 @@
 import { createResponse } from '@helper/http-api/response';
-import { log } from '@helper/logger';
 import { GalleryFile } from './gallery.file.js';
 import { DbService } from '../services/db-service';
 
@@ -11,29 +10,14 @@ export class GalleryManager {
   }
 
   async getGallery(user: string, pageNumber: number, pageLimit: number, dbService: DbService) {
-    let pagesAmount;
-    if (user) {
-      const userImagesNumber = await dbService.getUserImagesNumber(user, pageLimit);
-      pagesAmount = this.file.getNumberOfPagesForUser(userImagesNumber);
-    } else {
-      pagesAmount = await this.file.getNumberOfPages(pageLimit);
-    }
-
-    if (pageNumber > pagesAmount || pageNumber <= 0) {
-      log(`The page number ${pageNumber} is wrong.`);
+    const pagesAmount = await this.file.getNumberOfPages(pageLimit, dbService, user);
+    const isValid = this.file.isPagesAmountValid(pagesAmount, pageNumber);
+    if (!isValid) {
       return createResponse(400, {
-        message: `Page should be Greater than 0 and less than ${pagesAmount + 1}`,
+        message: `Page number should be greater than 0 and less than ${pagesAmount + 1}`,
       });
     }
-
-    if (user) {
-      log(`A user ${user} was specified.`);
-      const images = await dbService.getUserImages(pageNumber, pageLimit, pagesAmount, user);
-      log(`Got images for the user ${user}.`);
-      return createResponse(200, images);
-    } else {
-      const images = await dbService.getImages(pageNumber, pageLimit, pagesAmount);
-      return createResponse(200, images);
-    }
+    const images = await this.file.getImages(pageNumber, pageLimit, pagesAmount, dbService, user);
+    return createResponse(200, images);
   }
 }
